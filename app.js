82895c323a7c93330401baa92f541add1c089909
wangbo <bbw1996@qq.com>
2021-03-18 17:44:33 +0800

    update

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const logger = require('./logger')
 
-app.get('/', (request, response) => {
-  response.send('你好1 ');
-});
+const app = express()
+const port = 3000
+
+app.use(logger('dev:'))
+app.use((request, response, next) => {
+  if (request.path === '/' && request.method === 'get') {
+    response.send('根目录')
+  }
+  next()
+})
+app.get('/xxx', (request, response, next) => {
+  response.send('xxx 页面')
+  next()
+})
+
+app.route('/zzz')
+  .get((request, response, next) => {
+    response.send('zzz')
+  })
+  .post()
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+  console.log(`正在监听端口:${port}`)
+})
